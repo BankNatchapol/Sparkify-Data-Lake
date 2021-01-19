2280dbc6f9bb44995cc66116f82453b0c3b435cb
BankNatchapol <natchapol.pat@gmail.com>
2021-01-20 00:53:25 +0700

    add etl

--- etl.py
+++ etl.py
@@ -9,8 +9,8 @@ from pyspark.sql.functions import year, month, dayofmonth, hour, weekofyear, dat
 config = configparser.ConfigParser()
 config.read('dl.cfg')
 
-os.environ['AWS_ACCESS_KEY_ID']=config['AWS_ACCESS_KEY_ID']
-os.environ['AWS_SECRET_ACCESS_KEY']=config['AWS_SECRET_ACCESS_KEY']
+os.environ['AWS_ACCESS_KEY_ID']=config['SECRET']['AWS_ACCESS_KEY_ID']
+os.environ['AWS_SECRET_ACCESS_KEY']=config['SECRET']['AWS_SECRET_ACCESS_KEY']
 
 
 def create_spark_session():
@@ -23,72 +23,34 @@ def create_spark_session():
 
 def process_song_data(spark, input_data, output_data):
     # get filepath to song data file
-    song_data = 
+    song_data = input_data + "song_data"
     
     # read song data file
-    df = 
+    df = spark.read.json(song_data)
 
     # extract columns to create songs table
-    songs_table = 
+    songs_table = df.select("song_id", "title", "artist_id", "year", "duration").dropDuplicates("song_id")
     
     # write songs table to parquet files partitioned by year and artist
-    songs_table
+    songs_table.write.mode("overwrite").partitionBy("year", "artist_id").parquet(output_data + "songs")
 
     # extract columns to create artists table
-    artists_table = 
+    artists_table = df.selectExpr("artist_id", "artist_name as name", "artist_location as location", "artist_latitude as lattitude", "artist_longitude as longitude").dropDuplicates("artist_id")
     
     # write artists table to parquet files
-    artists_table
+    artists_table.write.mode("overwrite").parquet(output_data + "artists")
 
 
-def process_log_data(spark, input_data, output_data):
-    # get filepath to log data file
-    log_data =
 
-    # read log data file
-    df = 
-    
-    # filter by actions for song plays
-    df = 
-
-    # extract columns for users table    
-    artists_table = 
-    
-    # write users table to parquet files
-    artists_table
-
-    # create timestamp column from original timestamp column
-    get_timestamp = udf()
-    df = 
-    
-    # create datetime column from original timestamp column
-    get_datetime = udf()
-    df = 
-    
-    # extract columns to create time table
-    time_table = 
-    
-    # write time table to parquet files partitioned by year and month
-    time_table
-
-    # read in song data to use for songplays table
-    song_df = 
-
-    # extract columns from joined song and log datasets to create songplays table 
-    songplays_table = 
-
-    # write songplays table to parquet files partitioned by year and month
-    songplays_table
 
 
 def main():
     spark = create_spark_session()
     input_data = "s3a://udacity-dend/"
-    output_data = ""
+    output_data = "s3a://atharva-dend/data-lake/"
     
     process_song_data(spark, input_data, output_data)    
-    process_log_data(spark, input_data, output_data)
-
+    
 
 if __name__ == "__main__":
     main()
